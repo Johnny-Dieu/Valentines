3c0b5b1615112051be275c3701f648cd331a3de4
CodeSandbox <support@codesandbox.io>
2024-02-02 16:49:53 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,69 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
+const phrases = [
+  "No",
+  "Are You Sure?",
+  "Really?",
+  "Really Sure?",
+  "Stinky, You Sure?",
+  "I'm Gonna Cry... ;(",
+  "You're Breaking My Heart",
+];
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 20 + 16;
+
+  function handleNoClick() {
+    setNoCount(noCount + 1);
+  }
+
+  function getNoButtonText() {
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="valentine-container">
+      {yesPressed ? (
+        <>
+          <img
+            src="https://media1.tenor.com/m/4s5GO9wA0-4AAAAC/cute-cow.gif"
+            width="480"
+            height="439"
+            alt="Valentine's Day gif"
+          />
+          <div className="text">Yay! I'll pick you up 🫀</div>
+        </>
+      ) : (
+        <>
+          <img
+            src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExdTdrZ3pldjJiMmpsc2szYTE1dHVtMHE4OTVlaHF6cGV6bzVqbmo1NCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/6CAFIoo26LkJxjk3Gc/giphy.gif"
+            width="480"
+            height="439"
+            alt="Valentine's Day gif"
+          />
+          <div className="text">Will you be my valentine?</div>
+          <div>
+            <button
+              className="yesButton"
+              style={{ fontSize: yesButtonSize }}
+              onClick={() => setYesPressed(true)}
+            >
+              Yes
+            </button>
+            <button
+              className="noButton"
+              onClick={handleNoClick}
+              style={{ fontSize: yesButtonSize }}
+            >
+              {getNoButtonText()}
+            </button>
+          </div>
+        </>
+      )}
+    </div>
   );
 }
 
